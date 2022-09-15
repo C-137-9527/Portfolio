afea17d305e553952eb6c5c2d3f43ce7e265b94b
C-137-9527 <88403531+C-137-9527@users.noreply.github.com>
2022-09-15 16:59:03 -0700

    Delete react-students-filter directory

--- react-students-filter/src/components/Messages/StatusMessage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './Messages.module.css';
-
-const StatusMessage = ({ message }) => {
-  return <h2 className={classes.statusMessage}>{message}</h2>;
-};
-
-export default StatusMessage;
